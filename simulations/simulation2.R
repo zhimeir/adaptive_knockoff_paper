46b8a03bac45911d37ecc82c5e152434f53ed3e0
zhimeir <renzhimei@gmail.com>
2020-01-09 13:20:27 -0800

    Update simulation2.R

--- simulations/simulation2.R
+++ simulations/simulation2.R
@@ -135,7 +135,7 @@ writeMat(savedir,fdp = fdp,power = power,res = res)
 ####################################
 ## Adaknockoff with gam
 ####################################
-res = filter_gam(W,z,alpha =alphalist,offset=1,df=10)
+res = filter_gam(W,z,alpha =alphalist,offset=1,df=5,reveal_prop = 0.2)
 fdp = c()
 power = c()
 for (i in 1:length(alphalist)){
