1f51b605f97ac216e2a76f1b953bc9d4465ca5eb
zhimeir <renzhimei@gmail.com>
2020-01-08 17:00:27 -0800

    new

--- simulations/simulation2.R
+++ simulations/simulation2.R
@@ -181,7 +181,7 @@ writeMat(savedir,fdp = fdp,power = power,res = res)
 ####################################
 ## Adaknockoff with EM algorithm
 ####################################
-res = filter_EM(W,z,alpha =alphalist,offset=1,s0=0.01,df=2,mute=FALSE)
+res = filter_EM(W,z,alpha =alphalist,offset=1,s0=0.01,cutoff = 0)
 fdp = c()
 power = c()
 for (i in 1:length(alphalist )){
